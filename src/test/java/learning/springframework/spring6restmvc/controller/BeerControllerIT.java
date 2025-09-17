4fc9146764ec543e644fdfcec1309cf67ba9ca28
afiatarsyad <afiat.arsyad@gmail.com>
2025-09-17 18:32:06 +0700

    git pull

---
+++ src/test/java/learning/springframework/spring6restmvc/controller/BeerControllerIT.java
@@ -0,0 +1,44 @@
+package learning.springframework.spring6restmvc.controller;
+
+import jakarta.transaction.Transactional;
+import learning.springframework.spring6restmvc.model.BeerDTO;
+import learning.springframework.spring6restmvc.repositories.BeerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@SpringBootTest
+class BeerControllerIT {
+    @Autowired
+    BeerController beerController;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testListBeers() {
+
+        List<BeerDTO> dtos = beerController.listBeers();
+
+        assertThat(dtos.size()).isEqualTo(3);
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testEmptyList() {
+
+        beerRepository.deleteAll();
+
+        List<BeerDTO> dtos = beerController.listBeers();
+
+        assertThat(dtos.size()).isEqualTo(0);
+    }
+
+
+}
